d79f0230a040aea977f93b05671d4d7b78c400ff
Derek Schauland <derek@derekschauland.com>
2019-01-13 16:53:12 -0600

    Initial commit

---
+++ GoogleCloud.psm1
@@ -0,0 +1,19 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2019 v5.6.156
+	 Created on:   	1/13/2019 4:52 PM
+	 Created by:   	 Derek
+	 Organization: 	 
+	 Filename:     	GoogleCloud.psm1
+	-------------------------------------------------------------------------
+	 Module Name: GoogleCloud
+	===========================================================================
+#>
+
+
+
+function Write-HelloWorld
+{
+	Write-Host "Hello World"
+}
+
