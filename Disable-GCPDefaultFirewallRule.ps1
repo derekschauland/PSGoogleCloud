1964b014e2916247f87210c22c4f5423a387624d
Derek Schauland <derek@derekschauland.com>
2019-01-13 19:03:58 -0600

    More info to check for date on written file name

--- Disable-GCPDefaultFirewallRule.ps1
+++ Disable-GCPDefaultFirewallRule.ps1
@@ -36,18 +36,42 @@ Function Disable-GCPDefaultFirewallRule
         {
             gcloud config set project $item
             
-            #check for status of rule - if disabled check date in file and delete 
-            If ()
+            $rules = gcloud compute firewall-rules list
+            $rules | Out-File "$item-rules.json"
+            
+            $rulesobj = Get-Content "$item-rules.json" | ConvertFrom-Json
+            
+            ForEach ($fwrule In $rulesobj)
             {
+                If (($fwrule.name -match "default-allow-rdp") -and ($fwrule.disabled -eq $false))
+                {
+                    gcloud compute firewall-rules update default-allow-rdp --disabled
+                    
+                    "$fwrule.name-disabled-$(Get-Date -Format "yyyy-MM-dd")" | Out-File "$fwrule.name-disabled-$(Get-Date -Format "yyyy-MM-dd")".txt
+                    
+                    
+                }
+                ElseIf (($fwrule.name -match "default-allow-ssh") -and ($fwrule.disabled -eq $false))
+                {
+                    gcloud compute firewall-rules update default-allow-ssh --disabled
+                    "$fwrule.name-disabled-$(Get-Date -Format "yyyy-MM-dd")" | Out-File "$fwrule.name-disabled-$(Get-Date -Format "yyyy-MM-dd")"
+                }
                 
+                If ((Test-Path "$fwrule.name-disabled-*") -and (Get-ChildItem "$fwrule.name-disabled-*").Count -eq 1)
+                {
+                    #check date at end of file name for age 7 days older than today
+                    (Get-ChildItem "$fwrule.name-disabled-*").name.Substring((Get-ChildItem "$fwrule.name-disabled-*").name.Length - 10)
+                }
             }
-            gcloud compute firewall-rules update default-allow-rdp --disabled
+            #check for status of rule - if disabled check date in file and delete 
+            
+            
             #write file with project name, rule-name, date to bucket in gcp
             
             #check for file in bucket - if file exists, read file - if project and rule listed and date is older than 7 days from get date
             #delete rule
             
-            gcloud compute firewall-rules update default-allow-ssh --disabled
+            
             #write file with project name, rule-name, date to bucket in gcp
             
             #check for file in bucket - if file exists, read file - if project and rule listed and date is older than 7 days from get date
