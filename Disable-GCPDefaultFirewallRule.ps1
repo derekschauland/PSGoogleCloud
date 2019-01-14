235510624c89167b20442aac96ee71d58dddb272
Derek Schauland <derek@derekschauland.com>
2019-01-13 19:10:24 -0600

    removing the code to delete rule(s) disable only

--- Disable-GCPDefaultFirewallRule.ps1
+++ Disable-GCPDefaultFirewallRule.ps1
@@ -48,8 +48,7 @@ Function Disable-GCPDefaultFirewallRule
                     gcloud compute firewall-rules update default-allow-rdp --disabled
                     
                     "$fwrule.name-disabled-$(Get-Date -Format "yyyy-MM-dd")" | Out-File "$fwrule.name-disabled-$(Get-Date -Format "yyyy-MM-dd")".txt
-                    
-                    
+                        
                 }
                 ElseIf (($fwrule.name -match "default-allow-ssh") -and ($fwrule.disabled -eq $false))
                 {
@@ -57,11 +56,6 @@ Function Disable-GCPDefaultFirewallRule
                     "$fwrule.name-disabled-$(Get-Date -Format "yyyy-MM-dd")" | Out-File "$fwrule.name-disabled-$(Get-Date -Format "yyyy-MM-dd")"
                 }
                 
-                If ((Test-Path "$fwrule.name-disabled-*") -and (Get-ChildItem "$fwrule.name-disabled-*").Count -eq 1)
-                {
-                    #check date at end of file name for age 7 days older than today
-                    (Get-ChildItem "$fwrule.name-disabled-*").name.Substring((Get-ChildItem "$fwrule.name-disabled-*").name.Length - 10)
-                }
             }
             #check for status of rule - if disabled check date in file and delete 
             
